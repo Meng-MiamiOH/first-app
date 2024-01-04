8a7ef3ec8769915983e3bb5212276ea41b176325
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2024-01-04 14:24:42 -0500

    a better error handling

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,37 @@
-import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 
-export default function MealDetailsPage({params}) {
-  return <h1>Meal Details {params.mealSlug}</h1>;
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
